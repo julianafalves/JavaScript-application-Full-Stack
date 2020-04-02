5d0d48d2268252de5e622643b29ba850ace684be
julianafalves <49698966+julianafalves@users.noreply.github.com>
2020-04-02 15:10:14 -0300

    Add ONG data in incidents list

--- aulas/backend/src/controllers/IncidentController.js
+++ aulas/backend/src/controllers/IncidentController.js
@@ -8,14 +8,18 @@ module.exports = {
         const [count] = await connection('incidents').count() //contagem de quantos dados tem
 
         const incidents = await connection('incidents')
+            .join('ongs','ongs.id','=','incidents.ong_id') //seleciona os dados da ONG
+
             .limit(5) //pegar cinco casos por pagina
             .offset((page-1)*5)
-            .select("*")
             //o que foi feito a cima vai fazer com que seja apresentado cinco casos por pagina
             //para buscar a proxima pagina ficaria: http://localhost:3333/incidents?page=2
+            .select(["incidents.*",'ongs.name','ongs.email','ongs.whatsapp','ongs.city','ongs.uf']) 
+            //acima determina os campos que vamos utilizar da ONG
+            
         
         response.header('X-Total-Count',count['count(*)']) //apresentar no cabeçalho a quantidade de casos
-        
+
         return response.json(incidents)
     },
 
